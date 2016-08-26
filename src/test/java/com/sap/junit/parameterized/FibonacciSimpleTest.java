9e5a75a6505813e2db44ed2d7d255ea14f914d20
Dobreva <I044666@global.corp.sap>
2016-08-26 17:27:50 +0300

    try to break

--- src/test/java/com/sap/junit/parameterized/FibonacciSimpleTest.java
+++ src/test/java/com/sap/junit/parameterized/FibonacciSimpleTest.java
@@ -2,6 +2,7 @@ package com.sap.junit.parameterized;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -13,7 +14,7 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class FibonacciSimpleTest {
 	
-	public static String unusedVariable = "haha";
+	public String unusedVariable = "haha";
 	@Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] { { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 } });
@@ -27,7 +28,8 @@ public class FibonacciSimpleTest {
 		try {
 			fInput = input;
 			fExpected = expected;
-		} catch (RuntimeException ex ) {
+			throw new IOException();
+		} catch (IOException ex ) {
 			
 		}
 		
